d97a182df459e4123a34a384d58372ed2aeb6f2d
@Kerim <kerim.a@0xprocessing.com>
2024-12-22 21:15:10 +0500

    add directives to blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,6 @@
 <h1>About</h1>
 <p>This is a blog about Laravel</p>
+<p>{{ implode(', ', $tags) }}</p>
+@foreach ($team as $person)
+    <p>{{ $person['name'] }} - {{ $person['position'] }}</p>
+@endforeach
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,11 @@ Route::get('/', function () {
 });
 
 Route::get('about', function() {
-    return view('about');
+    $tags = ['обучение', 'программирование', 'php', 'laravel'];
+    $team = [
+        ['name' => 'Hodor', 'position' => 'programmer'],
+        ['name' => 'Joker', 'position' => 'CEO'],
+        ['name' => 'Elvis', 'position' => 'CTO'],
+    ];
+    return view('about', ['tags' => $tags, 'team' => $team]);
 });
